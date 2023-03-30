c125808307e45925a27dc3972ba6329b0a20441c
shivamak2003 <bhardwajshivam.2108@gmail.com>
2023-03-30 16:48:40 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="FaceBlur",
+    version="0.0.1",
+    author="SHivam",
+    emai="bhardwajshivam.2108@gmail.com",
+    packages=find_packages()
+    
+    
+)
